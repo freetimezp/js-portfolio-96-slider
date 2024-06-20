a7487536b7929f4df5e1b2874ef94aa8136cd6e6
freetimezp <kraftukrnet@ukr.net>
2024-06-20 22:11:05 +0300

    03. js-portfolio-96-slider. Slider.

---
+++ assets/js/main.js
@@ -0,0 +1,40 @@
+const listInfo = document.querySelector(".list-info");
+const listImg = document.querySelector(".list-img");
+const nextBtn = document.querySelector(".next-btn");
+const prevBtn = document.querySelector(".prev-btn");
+
+let index = 0;
+
+nextBtn.addEventListener("click", () => {
+    index = (index < 3) ? index + 1 : 3;
+
+    listInfo.style.transform = `translateY(${index * -25}%)`;
+    listImg.style.transform = `translateY(${index * -100}%)`;
+});
+
+prevBtn.addEventListener("click", () => {
+    index = (index > 0) ? index - 1 : 0;
+
+    listInfo.style.transform = `translateY(${index * -25}%)`;
+    listImg.style.transform = `translateY(${index * -100}%)`;
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
